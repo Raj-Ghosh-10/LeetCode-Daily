279c23b517ad941ef333111c1a81d1325dccbe91
Raj-Ghosh-10 <rajyajdhd123@gmail.com>
2025-07-01 22:10:12 +0530

    01-07-2025

--- JULY-2025/01-07-2025/p3330.java
+++ JULY-2025/01-07-2025/p3330.java
@@ -1,5 +1,12 @@
-package JULY-2025.01-07-2025;
-
-public - {
-    
+class Solution {
+    public int possibleStringCount(String word) {
+        int n=word.length();
+        int count=n;
+        for(int i=1;i<word.length();i++){
+            if(word.charAt(i)!=word.charAt(i-1)){
+                count--;
+            }
+        }
+        return count;
+    }
 }
