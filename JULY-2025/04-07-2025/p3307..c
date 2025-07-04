bfe5be96cc2df309aed304712ac3ad5fca9a114b
Raj-Ghosh-10 <rajyajdhd123@gmail.com>
2025-07-04 20:56:09 +0530

    04-07-2025

---
+++ JULY-2025/04-07-2025/p3307..c
@@ -0,0 +1,24 @@
+char kthCharacter(long long k, int* operations, int operationsSize) {
+    int hashbrown = 0; 
+    int t;
+
+    while (k != 1)
+    {
+        t = 63 - __builtin_clzll(k); 
+
+        if ((1LL << t) == k)
+        {
+            t--;
+        }
+
+        k -= (1LL << t);
+
+        if (operations[t] != 0)
+        {
+            hashbrown++;
+        }
+    }
+
+    char retval = (char)('a' + (hashbrown % 26));
+    return retval;
+}
