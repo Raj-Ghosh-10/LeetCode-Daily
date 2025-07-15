29ce8364919b7c701fbc06b3d75bf0f4190ff1ba
Raj-Ghosh-10 <rajyajdhd123@gmail.com>
2025-07-15 21:36:46 +0530

    15-07-2025

---
+++ JULY-2025/15-07-2025/p3136.rkt
@@ -0,0 +1,19 @@
+(define/contract (is-valid word)
+  (-> string? boolean?)
+  (let ((chars (string->list word))
+        (char-vowel? (lambda (c)
+                       (member
+                        (char-downcase c)
+                        (string->list "aeiou")))))
+    (and (> (string-length word) 2)
+         (andmap
+          (lambda (c)
+            (or (char-alphabetic? c)
+                (char-numeric? c)))
+          chars)
+         (ormap char-vowel? chars)
+         (ormap
+          (lambda (c)
+            (and (not (char-vowel? c))
+                 (not (char-numeric? c))))
+          chars))))
