d0499ce2b58d7cbb40f486248b381da14a772b98
Raj-Ghosh-10 <rajyajdhd123@gmail.com>
2025-07-22 22:39:25 +0530

    22-07-2025

---
+++ JULY-2025/22-07-2025/p1695.go
@@ -0,0 +1,17 @@
+func maximumUniqueSubarray(nums []int) int {
+	result := nums[0]
+	start := 0
+	windowSum := 0
+	inWindow := make(map[int]bool)
+	for end := range nums {
+		for inWindow[nums[end]] {
+			inWindow[nums[start]] = false
+			windowSum -= nums[start]
+			start++
+		}
+		windowSum += nums[end]
+		inWindow[nums[end]] = true
+		result = max(result, windowSum)
+	}
+	return result
+}
