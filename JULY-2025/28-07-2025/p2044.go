3e84f25c6600896f733cd76f63e86d9153c32c66
Raj-Ghosh-10 <rajyajdhd123@gmail.com>
2025-07-28 21:16:38 +0530

    28-07-2025

---
+++ JULY-2025/28-07-2025/p2044.go
@@ -0,0 +1,30 @@
+func countMaxOrSubsets(nums []int) int {
+    return NewSubsetOrTracker().Track(nums, 0, 0).MaxCount()
+}
+
+type SubsetOrTracker map[int]int
+
+func NewSubsetOrTracker() SubsetOrTracker {
+    s := make(map[int]int)
+    return s
+}
+
+func (s SubsetOrTracker) Track(nums []int, i, or int) SubsetOrTracker {
+    if i == len(nums) {
+        s[or]++
+        return s
+    }
+    _ = s.Track(nums, i+1, or|nums[i])
+    _ = s.Track(nums, i+1, or)
+    return s
+}
+
+func (s SubsetOrTracker) MaxCount() int {
+    var max, res int
+    for k, v := range s {
+        if k > max {
+            max, res = k, v
+        }
+    }
+    return res
+}
