a5b68a9a168a75da5485fbaff76d0e8001c68751
Raj-Ghosh-10 <rajyajdhd123@gmail.com>
2025-08-02 17:33:08 +0530

    02-08-2025

---
+++ AUGUST-2025/02-08-2025/p2561.ts
@@ -0,0 +1,45 @@
+function minCost(basket1: number[], basket2: number[]): number {
+    const count1 = new Map<number, number>();
+    const count2 = new Map<number, number>();
+    let minX = Infinity;
+    for (const x of basket1){
+        count1.set(x, (count1.get(x) || 0) + 1);
+        minX = Math.min(minX, x);
+    } 
+    for (const x of basket2){
+        count2.set(x, (count2.get(x) || 0) + 1);
+        minX = Math.min(minX, x);
+    }
+    for (const [key, val1] of count1.entries()){
+        const val2 = count2.get(key) || 0;
+        const minCount = Math.min(val1, val2);
+        count1.set(key, val1 - minCount);
+        count2.set(key, val2 - minCount);
+    }
+    const swap1: number[] = [];
+    const swap2: number[] = [];
+    for (const [key, val] of count1.entries()){
+        if (val % 2 !== 0){
+            return -1;
+        }
+        for (let i = 0; i < val / 2; i++){
+            swap1.push(key);
+        }
+    }
+    for (const [key, val] of count2.entries()){
+        if (val % 2 !== 0){
+            return -1;
+        }
+        for (let i = 0; i < val / 2; i++){
+            swap2.push(key);
+        }
+    }
+    swap1.sort((a, b) => a - b);
+    swap2.sort((a, b) => b - a);
+    let res = 0;
+    for (let i = 0; i < swap1.length; i++){
+        res += Math.min(swap1[i], swap2[i], 2 * minX);
+    }
+    return res;
+
+};
