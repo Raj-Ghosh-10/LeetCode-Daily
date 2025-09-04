2782280734fdd14340a3dc9c36225713f38b34ae
Raj-Ghosh-10 <rajyajdhd123@gmail.com>
2025-09-04 23:08:23 +0530

    04-09-2025

---
+++ SEPTEMBER-2025/04-09-2025/p3516.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int findClosest(int x, int y, int z) {
+        if(abs(z-x)==abs(z-y)){
+            return 0;
+        }
+        if(abs(z-x)<abs(z-y)){
+            return 1;
+        }
+        else{
+            return 2;
+        }
+    }
+};
