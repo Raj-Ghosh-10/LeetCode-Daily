b0a4c5ab848513bdefcf215127578d2e5799caa1
Raj-Ghosh-10 <rajyajdhd123@gmail.com>
2025-10-10 11:50:52 +0530

    10-10-2025

--- POTD.java
+++ POTD.java
@@ -1,18 +1,19 @@
 class Solution {
-    public long minTime(int[] skill, int[] mana) {
-        int n = skill.length, m = mana.length;
-        long[] acc = new long[n + 1];
-        for (int i = 0; i < n; i++) {
-            acc[i + 1] = acc[i] + skill[i];
-        }
-        long t = 0, t2 = 0;
-        for (int j = 1; j < m; j++) {
-            t2 = 0;
-            for (int i = 0; i < n; i++) {
-                t2 = Math.max(t2, t + mana[j - 1] * acc[i + 1] - mana[j] * acc[i]);
+    public int maximumEnergy(int[] energy, int k) {
+        
+        int n = energy.length;
+        int[] raj = new int[n];
+        int maxEnergy = Integer.MIN_VALUE;
+
+        for (int i = n - 1; i >= 0; i--) {
+            if (i + k < n) {
+                raj[i] = energy[i] + raj[i + k];
+            } else {
+                raj[i] = energy[i];
             }
-            t = t2;
+            maxEnergy = Math.max(maxEnergy, raj[i]);
         }
-        return t + mana[m - 1] * acc[n];
+
+        return maxEnergy;
     }
 }
