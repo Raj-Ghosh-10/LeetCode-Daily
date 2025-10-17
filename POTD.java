2754184ae99b0ee1a236f634d86145d49a68210e
Raj-Ghosh-10 <rajyajdhd123@gmail.com>
2025-10-17 19:14:46 +0530

    17-10-2025

--- POTD.java
+++ POTD.java
@@ -1,26 +1,42 @@
 class Solution {
-    public int findSmallestInteger(int[] nums, int value) {
-        int[] cnt = new int[value];
-        for (int x: nums){
-            int m = mod(x, value);
-            cnt[m]++;
+    Map<Long, Integer> bitMemo = new HashMap<>();
+
+    public int maxPartitionsAfterOperations(String s, int k) {
+        return solve(0, 0, true, s, k) + 1;
+    }
+
+    private int solve(int i, long uniquechars, boolean canchange, String s, int k) {
+
+        long key = ((long)i << 27) | ((uniquechars) << 1) | (canchange ? 1L : 0L);
+        if (bitMemo.containsKey(key)) return bitMemo.get(key);
+
+        if (i == s.length()) return 0;
+
+        int charidx = s.charAt(i) - 'a';
+        long newunique = uniquechars | (1L << charidx);
+        int uniquecount = Long.bitCount(newunique);
+
+        int result;
+        if (uniquecount > k) {
+            result = 1 + solve(i + 1, (1L << charidx), canchange, s, k);
+        } else {
+            result = solve(i + 1, newunique, canchange, s, k);
         }
-        int i = 0;
-        while (true){
-            int m = i % value;
-            if (cnt[m] > 0) {
-                cnt[m]--;
-                i++;
-            }else {
-                return i;
+
+        if (canchange) {
+            for (int newidx = 0; newidx < 26; newidx++) {
+                long newcharset = uniquechars | (1L << newidx);
+                int newuniquecount = Long.bitCount(newcharset);
+
+                if (newuniquecount > k) {
+                    result = Math.max(result, 1 + solve(i + 1, (1L << newidx), false, s, k));
+                } else {
+                    result = Math.max(result, solve(i + 1, newcharset, false, s, k));
+                }
             }
         }
-    }
-    public int mod(int num, int m){
-        int raj = num % m;
-        if (raj < 0){
-            raj += m;
-        } 
-        return raj;
+
+        bitMemo.put(key, result);
+        return result;
     }
 }
