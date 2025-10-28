a59f14b7b515b9ee235b43d2fdbbf18e3ffaa005
Raj-Ghosh-10 <rajyajdhd123@gmail.com>
2025-10-28 07:49:50 +0530

    28-10-2025

--- POTD.java
+++ POTD.java
@@ -1,11 +1,19 @@
 class Solution {
-    public boolean hasSameDigits(String s) {
-        char[] a = s.toCharArray();
-        for (int i = a.length - 1; i >= 2; i--) {
-            for (int j = 0; j < i; j++) {
-                a[j] = (char) ((a[j] + a[j + 1]) % 10 + '0');
+    public int countValidSelections(int[] nums) {
+        int count = 0, len = nums.length;
+        int sum = 0;
+        for (int num : nums) sum += num;
+        int halfSum = 0;
+        for (int i = 0; i < len; i++) {
+            halfSum += nums[i];
+            if (nums[i] == 0) {
+                if (2 * halfSum == sum) {
+                    count += 2;
+                } else if (Math.abs(sum - 2*halfSum) == 1) {
+                    count++;
+                } 
             }
         }
-        return a[0] == a[1];
+        return count;
     }
 }
