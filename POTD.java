670fcbabdfdc3a050e628fa7af147f184b898cae
Raj-Ghosh-10 <rajyajdhd123@gmail.com>
2025-10-29 19:27:35 +0530

    29-10-2025

--- POTD.java
+++ POTD.java
@@ -1,19 +1,10 @@
 class Solution {
-    public int countValidSelections(int[] nums) {
-        int count = 0, len = nums.length;
-        int sum = 0;
-        for (int num : nums) sum += num;
-        int halfSum = 0;
-        for (int i = 0; i < len; i++) {
-            halfSum += nums[i];
-            if (nums[i] == 0) {
-                if (2 * halfSum == sum) {
-                    count += 2;
-                } else if (Math.abs(sum - 2*halfSum) == 1) {
-                    count++;
-                } 
-            }
+    public int smallestNumber(int n) {
+        int num = 2, temp = n;
+        while (temp > 0) {
+            num <<= 1;
+            temp >>= 1;
         }
-        return count;
+        return (num >> 1) - 1;
     }
 }
