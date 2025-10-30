aea1f230b6e72eb2a416b50e7b2c0541d100ac2c
Raj-Ghosh-10 <rajyajdhd123@gmail.com>
2025-10-30 10:54:14 +0530

    30-10-2025

--- OCTOBER-2025/30-10-2025/p1526.py
+++ OCTOBER-2025/30-10-2025/p1526.py
@@ -1,3 +1,5 @@
+#python
+
 class Solution(object):
     def minNumberOperations(self, target):
         last =0
@@ -7,3 +9,17 @@ class Solution(object):
                 ops += i-last
             last = i
         return ops
+
+
+#python3
+
+class Solution:
+    def minNumberOperations(self, target: List[int]) -> int:
+        ans = power = prev = 0
+        for i in target:
+            if i >= prev:
+                power = i - prev
+                ans += power
+            prev = i
+
+        return ans
