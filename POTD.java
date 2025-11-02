bd5deff438edd59c4a7a164aa51c26f055e64c6a
Raj-Ghosh-10 <rajyajdhd123@gmail.com>
2025-11-02 22:21:14 +0530

    02-11-2025

--- POTD.java
+++ POTD.java
@@ -1,25 +1,35 @@
 class Solution {
-    public ListNode modifiedList(int[] nums, ListNode head) {
-        int max = -1;
-        for(int num : nums ){
-            max = num > max ? num : max;
+    public int countUnguarded(int m, int n, int[][] guards, int[][] walls) {
+        int[][] arr = new int[m][n];
+        for (int[] w : walls)
+            arr[w[0]][w[1]] = -1;
+        for (int[] g : guards)
+            arr[g[0]][g[1]] = -1;
+        for (int[] g : guards)
+            populate(g[0], g[1], arr);
+        int ans = 0;
+        for (int[] row : arr)
+            for (int cell : row)
+                if (cell == 0)
+                    ans++;
+        return ans;
+    }
+    private void populate(int a, int b, int[][] arr) {
+        for (int i = a - 1; i >= 0; i--) {
+            if (arr[i][b] < 0) break;
+            arr[i][b] = 1;
         }
-        boolean[] freq = new boolean[max+1];
-
-        for(int num : nums) freq[num] = true;
-
-        ListNode temp = new ListNode();
-        ListNode current = temp;
-
-        while(head != null){
-            if( head.val >= freq.length || freq[head.val] == false){
-                current.next = head;
-                current = current.next;
-            }
-            head = head.next;
+        for (int i = a + 1; i < arr.length; i++) {
+            if (arr[i][b] < 0) break;
+            arr[i][b] = 1;
         }
-
-        current.next = null;
-        return temp.next;
+        for (int j = b - 1; j >= 0; j--) {
+            if (arr[a][j] < 0) break;
+            arr[a][j] = 1;
+        }
+        for (int j = b + 1; j < arr[0].length; j++) {
+            if (arr[a][j] < 0) break;
+            arr[a][j] = 1;
+        }
     }
 }
