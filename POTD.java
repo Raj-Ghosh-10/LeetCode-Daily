eb8d4673edc19ca8675f2820f0c8a1dc571bfa18
Raj-Ghosh-10 <rajyajdhd123@gmail.com>
2025-11-03 07:24:17 +0530

    03-11-2025

--- POTD.java
+++ POTD.java
@@ -1,35 +1,19 @@
 class Solution {
-    public int countUnguarded(int m, int n, int[][] guards, int[][] walls) {
-        int[][] arr = new int[m][n];
-        for (int[] w : walls)
-            arr[w[0]][w[1]] = -1;
-        for (int[] g : guards)
-            arr[g[0]][g[1]] = -1;
-        for (int[] g : guards)
-            populate(g[0], g[1], arr);
-        int ans = 0;
-        for (int[] row : arr)
-            for (int cell : row)
-                if (cell == 0)
-                    ans++;
-        return ans;
-    }
-    private void populate(int a, int b, int[][] arr) {
-        for (int i = a - 1; i >= 0; i--) {
-            if (arr[i][b] < 0) break;
-            arr[i][b] = 1;
+    public int minCost(String colors, int[] neededTime) {
+        char[] arr = colors.toCharArray();
+        int res = 0;
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] == arr[i - 1]) {
+                int total = neededTime[i - 1];
+                int max = neededTime[i - 1];
+                while (i < arr.length && arr[i] == arr[i - 1]) {
+                    total += neededTime[i];
+                    max = Math.max(max, neededTime[i]);
+                    i++;
+                }
+                res += total - max;
+            }
         }
-        for (int i = a + 1; i < arr.length; i++) {
-            if (arr[i][b] < 0) break;
-            arr[i][b] = 1;
-        }
-        for (int j = b - 1; j >= 0; j--) {
-            if (arr[a][j] < 0) break;
-            arr[a][j] = 1;
-        }
-        for (int j = b + 1; j < arr[0].length; j++) {
-            if (arr[a][j] < 0) break;
-            arr[a][j] = 1;
-        }
+        return res;
     }
 }
