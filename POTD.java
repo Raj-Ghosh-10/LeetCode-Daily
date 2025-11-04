3dbeffeb0f995f9561501817f7c7be94c4f78cf0
Raj-Ghosh-10 <rajyajdhd123@gmail.com>
2025-11-04 12:10:36 +0530

    04-11-2025

--- POTD.java
+++ POTD.java
@@ -1,19 +1,40 @@
 class Solution {
-    public int minCost(String colors, int[] neededTime) {
-        char[] arr = colors.toCharArray();
-        int res = 0;
-        for (int i = 1; i < arr.length; i++) {
-            if (arr[i] == arr[i - 1]) {
-                int total = neededTime[i - 1];
-                int max = neededTime[i - 1];
-                while (i < arr.length && arr[i] == arr[i - 1]) {
-                    total += neededTime[i];
-                    max = Math.max(max, neededTime[i]);
-                    i++;
+    public int[] findXSum(int[] nums, int k, int x) {
+        int[] result = new int[nums.length - k + 1];
+        for (int i = 0; i < result.length; i++) {
+            int left = i, right = i + k - 1;
+            result[i] = findXSumofSubArray(nums, left, right, x);
+        }
+        return result;
+    }
+    private int findXSumofSubArray(int[] nums, int left, int right, int x) {
+        int sum = 0, distinctCount = 0;
+        int[] freq = new int[51]; 
+        for (int i = left; i <= right; i++) {
+            sum += nums[i];
+            if (freq[nums[i]] == 0) {
+                distinctCount++;
+            }
+            freq[nums[i]]++;
+        }
+        if (distinctCount < x) {
+            return sum;
+        }
+        sum = 0;
+        for (int pick = 0; pick < x; pick++) {
+            int bestFreq = -1;
+            int bestVal = -1;
+            for (int val = 50; val >= 1; val--) {
+                if (freq[val] > bestFreq) {
+                    bestFreq = freq[val];
+                    bestVal = val;
                 }
-                res += total - max;
+            }
+            if (bestVal != -1) {
+                sum += bestVal * bestFreq;
+                freq[bestVal] = 0;
             }
         }
-        return res;
+        return sum;
     }
 }
