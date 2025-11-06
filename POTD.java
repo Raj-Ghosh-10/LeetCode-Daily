ae1e6b8243e277754d5a00061abcf84afbc1fdb6
Raj-Ghosh-10 <rajyajdhd123@gmail.com>
2025-11-06 06:32:40 +0530

    06-11-2025

--- POTD.java
+++ POTD.java
@@ -1,55 +1,76 @@
 class Solution {
-    private int x;
-    private long sum = 0L;
-    private Map<Integer, Integer> freq;
-    private final TreeSet<int[]> active = new TreeSet<>((a, b) -> a[0] == b[0] ? a[1] - b[1] : a[0] - b[0]), inactive = new TreeSet<>((a, b) -> a[0] == b[0] ? a[1] - b[1] : a[0] - b[0]);
-    public long[] findXSum(int[] nums, int k, int x) {
-        int n = nums.length;
-        this.x = x;
-        freq = new HashMap<>(n);
-        long[] ans = new long[n - k + 1];
-        for(int i = 0; i < n; i++) {
-            int count = freq.merge(nums[i], 1, Integer::sum);
-            remove(count - 1, nums[i]);
-            add(count, nums[i]);
-            if(i + 1 >= k) {
-                ans[i - k + 1] = sum;
-                count = freq.merge(nums[i - k + 1], -1, Integer::sum);
-                remove(count + 1, nums[i - k + 1]);
-                add(count, nums[i - k + 1]);
+
+    public void dfs(List<List<Integer>> arr, int [] memo, int c,  int u) {
+        memo[u] = c;
+
+        for (int v : arr.get(u)) {
+            if (memo[v] == c) continue;
+            dfs(arr, memo, c, v);
+        }
+    }
+ 
+    public int[] processQueries(int n, int[][] edges, int[][] queries) {
+
+        int m = edges.length;
+
+        List<List<Integer>> arr = new ArrayList<>();
+        for (int i = 0; i <= n; i++) arr.add(new ArrayList<>());
+
+        for (int [] e: edges) {
+            arr.get(e[0]).add(e[1]);
+            arr.get(e[1]).add(e[0]);
+        }  
+
+        int [] memo = new int[n + 1];
+        int cur = 0;
+
+        for (int i = 1; i <= n; i++) {
+            if (memo[i] != 0) continue;
+            cur++;
+            dfs(arr, memo, cur, i);
+        }
+
+        List<List<Integer>> g = new ArrayList<>();
+        for (int i = 0; i <= cur; i++) g.add(new ArrayList<>());
+
+        for (int i = 1; i <= n; i++) {
+            g.get(memo[i]).add(i);
+        }
+
+        int [] pts = new int[cur + 1];
+        boolean [] off = new boolean[n + 1];
+
+        List<Integer> ans1 = new ArrayList<>();
+
+        for (int i = 0; i < queries.length; i++) {
+            if (queries[i][0] == 2) {
+                off[queries[i][1]] = true;
+                continue;
+            } 
+                
+            if (!off[queries[i][1]]) {
+                ans1.add(queries[i][1]);
+                continue;
+            } 
+            
+            int gr = memo[queries[i][1]];
+            List<Integer> group = g.get(gr);
+
+            int pt = pts[gr];
+            while (pt < group.size() && off[group.get(pt)]) pt++;
+
+            if (pt >= group.size()) {
+                ans1.add(-1);
+            } else {
+                ans1.add(group.get(pt));
             }
+            
+            pts[gr] = pt;
         }
+
+        int [] ans = new int[ans1.size()];
+        for (int i = 0; i < ans1.size(); i++) ans[i] = ans1.get(i);
+
         return ans;
-    }
-    private void add(int count, int num) {
-        if(count == 0) return;
-        int[] val = new int[] {count, num};
-        if(active.size() < x) {
-            active.add(val);
-            sum += (long)count * num;
-            return;
-        }
-        int[] temp = active.first();
-        if(temp[0] > count || temp[0] == count && temp[1] >= num) {
-            inactive.add(val);
-            return;
-        }
-        sum += (long)count * num - (long)temp[0] * temp[1];
-        inactive.add(active.pollFirst());
-        active.add(val);
-    }
-    private void remove(int count, int num) {
-        if(count == 0) return;
-        int[] val = new int[] {count, num};
-        if(inactive.contains(val)) {
-            inactive.remove(val);
-            return;
-        }
-        active.remove(val);
-        sum -= (long)count * num;
-        if(inactive.isEmpty()) return;
-        int[] temp = inactive.pollLast();
-        sum += (long)temp[0] * temp[1];
-        active.add(temp);
     }
 }
