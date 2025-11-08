cc776565ac15302ec653a499a38e8041faa4526e
Raj-Ghosh-10 <rajyajdhd123@gmail.com>
2025-11-08 06:31:17 +0530

    08-11-2025

---
+++ NOVEMBER-2025/08-11-2025/1611.ts
@@ -0,0 +1,17 @@
+function minimumOneBitOperations(n: number): number {
+    let bin = n.toString(2).split('').reverse() // bit array stack
+
+    let result = 0
+
+    while (bin.length) {
+        const isOne = bin.pop() === '1'
+        if (isOne) {
+            // add amount of steps we know are after this one
+            result += 1 << (bin.length )
+            // flip the next bit
+            bin[bin.length - 1] = bin[bin.length - 1] === '1' ? '0' : '1'
+        }
+    }
+
+    return result
+};
