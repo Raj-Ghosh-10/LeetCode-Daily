e6c6439ad9970642c9d6ef3688199a5e373fedbb
Raj-Ghosh-10 <rajyajdhd123@gmail.com>
2025-11-09 06:00:10 +0530

    09-11-2025

--- POTD.java
+++ POTD.java
@@ -1,9 +1,12 @@
 class Solution {
-    public int minimumOneBitOperations(int n) {
-        int res =0;
-        while(n > 0){
-            res  = res ^ n;
-            n = n>>1;
+    public int countOperations(int num1, int num2) {
+        int res = 0;
+        while (num1 != 0 && num2 != 0) {
+            res += num1 / num2;
+            num1 %= num2;
+            int temp = num1;
+            num1 = num2;
+            num2 = temp;
         }
         return res;
     }
