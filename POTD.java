2e55cc909786c71f8419360d73b25d81d4aca3f8
Raj-Ghosh-10 <rajyajdhd123@gmail.com>
2025-11-10 05:43:07 +0530

    10-11-2025

--- POTD.java
+++ POTD.java
@@ -1,13 +1,17 @@
 class Solution {
-    public int countOperations(int num1, int num2) {
-        int res = 0;
-        while (num1 != 0 && num2 != 0) {
-            res += num1 / num2;
-            num1 %= num2;
-            int temp = num1;
-            num1 = num2;
-            num2 = temp;
+    public int minOperations(int[] nums) {
+        var stack = new int[nums.length + 1];
+        var top = 0;
+        var ans = 0;
+        for (var i = 0; i < nums.length; i++) {
+            while (stack[top] > nums[i]) {
+                top--;
+                ans++;
+            }
+            if (stack[top] != nums[i]){
+                stack[++top] = nums[i];
+            }
         }
-        return res;
+        return ans + top;
     }
 }
