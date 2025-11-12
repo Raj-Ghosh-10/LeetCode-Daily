57347621f9638ce7751ba1b9e026e6d489692d9f
Raj-Ghosh-10 <rajyajdhd123@gmail.com>
2025-11-12 05:46:22 +0530

    12-11-2025

--- POTD.java
+++ POTD.java
@@ -1,21 +1,41 @@
 class Solution {
-    public int findMaxForm(String[] S, int M, int N) {
-        int[][] dp = new int[M+1][N+1];
-        for (String str : S) {
-            int zeros = 0, ones = 0;
-            for (char c : str.toCharArray()){
-                if (c == '0') { 
-                    zeros++;
-                } else {
-                    ones++;
-                }
+    public int minOperations(int[] nums) {
+        int n = nums.length;
+        int num1 = 0;
+        int g = 0;
+        for (int x : nums) {
+            if (x == 1) {
+                num1++;
             }
-            for (int i = M; i >= zeros; i--){
-                for (int j = N; j >= ones; j--){
-                    dp[i][j] = Math.max(dp[i][j], dp[i-zeros][j-ones] + 1);
+            g = gcd(g, x);
+        }
+        if (num1 > 0) {
+            return n - num1;
+        }
+        if (g > 1) {
+            return -1;
+        }
+
+        int minLen = n;
+        for (int i = 0; i < n; i++) {
+            int currentGcd = 0;
+            for (int j = i; j < n; j++) {
+                currentGcd = gcd(currentGcd, nums[j]);
+                if (currentGcd == 1) {
+                    minLen = Math.min(minLen, j - i + 1);
+                    break;
                 }
             }
         }
-        return dp[M][N];
+        return minLen + n - 2;
+    }
+
+    private int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
     }
 }
