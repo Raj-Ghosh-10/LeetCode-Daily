dde0a44e5002d6adccf0880817f19e83010291dc
Raj-Ghosh-10 <rajyajdhd123@gmail.com>
2025-11-14 05:45:53 +0530

    14-11-2025

--- POTD.java
+++ POTD.java
@@ -1,17 +1,28 @@
 class Solution {
-    public int maxOperations(String s) {
-        int index = s.length() - 1, res = 0, running = 0;
-        while(index >= 0){
-            while(s.charAt(index) == '1'){
-                index--;
-                res += running;
-                if(index < 0) return res;
+    public int[][] rangeAddQueries(int n, int[][] Q) {
+        int[][] res = new int[n][n];
+        for (var q : Q) {
+            int r0 = q[0], c0 = q[1], r1 = q[2]+1, c1 = q[3]+1;
+            res[r0][c0]++;
+            if (c1 < n){ 
+                res[r0][c1]--;
             }
-            while(s.charAt(index) == '0'){
-                index--;
-                if(index < 0) return res;
-            } 
-            running++;
+            if (r1 < n) {
+                res[r1][c0]--;
+                if (c1 < n){
+                    res[r1][c1]++;
+                }
+            }
+        }
+        for (int i = 0; i < n; i++){
+            for (int j = 1; j < n; j++){
+            res[i][j] += res[i][j-1];
+            }
+        }
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < n; j++){
+            res[i][j] += res[i-1][j];
+            }
         }
         return res;
     }
