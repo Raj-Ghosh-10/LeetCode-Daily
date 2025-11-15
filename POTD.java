a054459fc312f78d37418d55c663b7dc24a9020c
Raj-Ghosh-10 <rajyajdhd123@gmail.com>
2025-11-15 06:34:24 +0530

    15-11-2025

--- POTD.java
+++ POTD.java
@@ -1,27 +1,26 @@
 class Solution {
-    public int[][] rangeAddQueries(int n, int[][] Q) {
-        int[][] res = new int[n][n];
-        for (var q : Q) {
-            int r0 = q[0], c0 = q[1], r1 = q[2]+1, c1 = q[3]+1;
-            res[r0][c0]++;
-            if (c1 < n){ 
-                res[r0][c1]--;
+    public int numberOfSubstrings(String s) {
+        int n = s.length();
+        int[] pre = new int[n + 1];
+        pre[0] = -1;
+        for (int i = 0; i < n; i++) {
+            if (i == 0 || (i > 0 && s.charAt(i - 1) == '0')) {
+                pre[i + 1] = i;
+            } else {
+                pre[i + 1] = pre[i];
             }
-            if (r1 < n) {
-                res[r1][c0]--;
-                if (c1 < n){
-                    res[r1][c1]++;
+        }
+        int res = 0;
+        for (int i = 1; i <= n; i++) {
+            int cnt0 = s.charAt(i - 1) == '0' ? 1 : 0;
+            int j = i;
+            while (j > 0 && cnt0 * cnt0 <= n) {
+                int cnt1 = (i - pre[j]) - cnt0;
+                if (cnt0 * cnt0 <= cnt1) {
+                    res += Math.min(j - pre[j], cnt1 - cnt0 * cnt0 + 1);
                 }
-            }
-        }
-        for (int i = 0; i < n; i++){
-            for (int j = 1; j < n; j++){
-            res[i][j] += res[i][j-1];
-            }
-        }
-        for (int i = 1; i < n; i++) {
-            for (int j = 0; j < n; j++){
-            res[i][j] += res[i-1][j];
+                j = pre[j];
+                cnt0++;
             }
         }
         return res;
