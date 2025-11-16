3f4f2c87b13b7f6e1acda0501780e0969aaccf01
Raj-Ghosh-10 <rajyajdhd123@gmail.com>
2025-11-16 17:29:45 +0530

    16-11-2025

--- POTD.java
+++ POTD.java
@@ -1,27 +1,18 @@
 class Solution {
-    public int numberOfSubstrings(String s) {
-        int n = s.length();
-        int[] pre = new int[n + 1];
-        pre[0] = -1;
-        for (int i = 0; i < n; i++) {
-            if (i == 0 || (i > 0 && s.charAt(i - 1) == '0')) {
-                pre[i + 1] = i;
-            } else {
-                pre[i + 1] = pre[i];
-            }
+    static {
+        for (int i = 0; i < 500; i++) {
+            numSub("111111");
         }
+    }
+    public static int numSub(String s) {
+        final int MODULO = 1000000007;
+        int onesStreak = 0 ;
         int res = 0;
-        for (int i = 1; i <= n; i++) {
-            int cnt0 = s.charAt(i - 1) == '0' ? 1 : 0;
-            int j = i;
-            while (j > 0 && cnt0 * cnt0 <= n) {
-                int cnt1 = (i - pre[j]) - cnt0;
-                if (cnt0 * cnt0 <= cnt1) {
-                    res += Math.min(j - pre[j], cnt1 - cnt0 * cnt0 + 1);
-                }
-                j = pre[j];
-                cnt0++;
+        for(char ch : s.toCharArray()) {
+            if (ch  == '1') {
+                res = ( res + ++onesStreak) % MODULO ;
             }
+            else    onesStreak = 0 ;
         }
         return res;
     }
