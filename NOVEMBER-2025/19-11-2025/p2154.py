cb5441dc8fac11ce754b7de9001be815894c3cb8
Raj-Ghosh-10 <rajyajdhd123@gmail.com>
2025-11-19 18:12:35 +0530

    19-11-2025

---
+++ NOVEMBER-2025/19-11-2025/p2154.py
@@ -0,0 +1,8 @@
+class Solution:
+    def findFinalValue(self, nums: List[int], original: int) -> int:
+        
+        res = original
+        
+        while res in nums:
+            res *= 2
+        return res
