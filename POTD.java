bdd32a65c744a4a56c9b2b681d99a4f40fa96a3e
Raj-Ghosh-10 <rajyajdhd123@gmail.com>
2025-11-20 07:20:13 +0530

    20-11-2025

--- POTD.java
+++ POTD.java
@@ -1,14 +1,31 @@
 class Solution {
-    public int findFinalValue(int[] nums, int k) {
-        int bits = 0;
-        for (int num : nums) {
-            if (num % k != 0) continue;
-            int n = num / k;
-            if ((n & (n - 1)) == 0){
-                bits |= n;
+    public int intersectionSizeTwo(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[1], b[1]));
+        int prev1 = intervals[0][1] - 1;
+        int prev2 = intervals[0][1];
+        int count = 2;
+        for (int i = 1; i < intervals.length; i++) {
+            int start = intervals[i][0];
+            int end   = intervals[i][1];
+            if (prev2 < start) {
+                prev1 = end - 1;
+                prev2 = end;
+                count += 2;
+            }
+            else if (prev1 < start) {
+                if (end == prev2) {
+                    prev1 = end - 1;
+                } else {
+                    prev1 = end;
+                }
+                if (prev1 > prev2) {
+                    int temp = prev1;
+                    prev1 = prev2;
+                    prev2 = temp;
+                }
+                count += 1;
             }
         }
-        bits++;
-        return k * (bits & -bits);
+        return count;
     }
 }
