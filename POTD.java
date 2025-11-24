7f948e292b7b5bd74942ef8c28db13c2de0c97f8
Raj-Ghosh-10 <rajyajdhd123@gmail.com>
2025-11-24 06:26:13 +0530

    24-11-2025

--- POTD.java
+++ POTD.java
@@ -1,37 +1,13 @@
 class Solution {
-    public int maxSumDivThree(int[] nums) {
-        int sum = 0;
-        int m1a = Integer.MAX_VALUE, m1b = Integer.MAX_VALUE;
-        int m2a = Integer.MAX_VALUE, m2b = Integer.MAX_VALUE;
+    public List<Boolean> prefixesDivBy5(int[] nums) {
+        List<Boolean> ans = new ArrayList<>();
+        int mod = 0;
 
-        for (int x : nums) {
-            sum += x;
-            int r = x % 3;
-
-            if (r == 1) {
-                if (x < m1a) { m1b = m1a; m1a = x; }
-                else if (x < m1b) m1b = x;
-            } else if (r == 2) {
-                if (x < m2a) { m2b = m2a; m2a = x; }
-                else if (x < m2b) m2b = x;
-            }
+        for (int bit : nums) {
+            mod = (mod * 2 + bit) % 5;
+            ans.add(mod == 0);
         }
 
-        int rem = sum % 3;
-        if (rem == 0) return sum;
-
-        int remove = Integer.MAX_VALUE;
-
-        if (rem == 1) {
-            remove = Math.min(m1a, 
-                     (m2a == Integer.MAX_VALUE || m2b == Integer.MAX_VALUE) ? 
-                       Integer.MAX_VALUE : m2a + m2b);
-        } else {
-            remove = Math.min(m2a, 
-                     (m1a == Integer.MAX_VALUE || m1b == Integer.MAX_VALUE) ? 
-                       Integer.MAX_VALUE : m1a + m1b);
-        }
-
-        return remove == Integer.MAX_VALUE ? 0 : sum - remove;
+        return ans;
     }
 }
